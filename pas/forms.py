3842754ffb739ea26ba37b260bcab2ce9c6b1d96
Siimon13 <chen.simon89@gmail.com>
2017-02-18 06:38:32 -0500

    Basic Prototype

---
+++ pas/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class UserForm(forms.Form):
+    first_name = forms.CharField(max_length = 200, required = False)
+    last_name = forms.CharField(max_length = 200, required = False)
+    ethnicity = forms.CharField(max_length = 200, required = False)
+    age = forms.CharField(max_length = 200, required = False)
+    lifestyle = forms.CharField(max_length = 200, required = False)
+    gender = forms.CharField(max_length = 200, required = False)
+    current_weight = forms.CharField(max_length = 200, required = False)
+    goal_weight = forms.CharField(max_length = 200, required = False)
+    
--- pas/models.py
+++ pas/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    first_name = models.CharField(max_length = 200, null = False)
+    last_name = models.CharField(max_length = 200, null = False)
+    ethnicity = models.CharField(max_length = 200, null = False)
+    gender = models.CharField(max_length = 200, null = False)
+    current_weight = models.CharField(max_length = 200, null = False)
+    goal_weight = models.CharField(max_length = 200, null = False)
